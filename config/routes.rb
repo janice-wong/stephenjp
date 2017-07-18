ed28e3065af2e70ce8410e51c1ef0b86d0904db8
Janice Wong <JaniceWong13@gmail.com>
2017-07-18 16:11:03 -0400

    Completed feature page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/' => 'pages#index'
+  get '/about' => 'pages#about'
   get '/contact' => 'pages#contact'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
