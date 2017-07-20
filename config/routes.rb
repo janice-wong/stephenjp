11f42197c25d9b960afb122a439d8c7d02fc62bb
Janice Wong <JaniceWong13@gmail.com>
2017-07-20 15:14:37 -0400

    Installed fancybox, font-awesome, and updated filmography page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/' => 'pages#index'
+  get '/filmography' => 'pages#filmography'
   get '/about' => 'pages#about'
   get '/contact' => 'pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
