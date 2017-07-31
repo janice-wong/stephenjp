9bae2820e292c3a4a837109e9bb28fb132dad86c
Janice Wong <JaniceWong13@gmail.com>
2017-07-31 09:51:08 -0400

    Complete events page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,15 @@
 class PagesController < ApplicationController
+  def index
+    if params[:cat] == 'portraits'
+      render 'portraits.html.erb'
+    elsif params[:cat] == 'dance'
+      render 'dance.html.erb'
+    elsif params[:cat] == 'events'
+      render 'events.html.erb'
+    elsif params[:cat] == 'street'
+      render 'street.html.erb'
+    else
+      render 'index.html.erb'
+    end
+  end
 end
